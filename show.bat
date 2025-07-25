e074a832900e4797d257794061f0b422c7d16986
edson22-pl <spellingbee05@gmail.com>
2025-07-25 11:28:23 -0300

    Update show.bat

--- show.bat
+++ show.bat
@@ -1,5 +1,8 @@
 @echo off
-start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+REM Desativa o fail-safe do PyAutoGUI temporariamente
+python -c "import pyautogui; pyautogui.FAILSAFE = False"
+
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig.exe"
 python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
 python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
 python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
